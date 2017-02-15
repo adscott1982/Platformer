dfa4c07bb057af61e149f270c97ec669b0bd654f
Andrew Scott <adscott1982@googlemail.com>
2017-02-15 22:54:35 +0000

    First commit.

---
+++ Assets/BoneToPhysics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneToPhysics : MonoBehaviour
+{
+    Rigidbody2D body;
+    Transform lastTransform;
+
+    // Use this for initialization
+    void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        this.lastTransform = this.transform;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        var translation = this.transform.position - lastTransform.position;
+        var rotation = this.transform.rotation.eulerAngles.z;
+        this.body.MovePosition(this.transform.position + translation);
+        this.body.MoveRotation(rotation);
+
+        
+        this.lastTransform = this.transform;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    Transform exhaust;
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        var translation = Input.GetAxisRaw("Horizontal") * speed;
+
+        this.transform.position += new Vector3(translation, 0, 0) * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
